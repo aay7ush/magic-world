3f567456ec5d04ab9bb041a467d3e2a4cad50cb8
Aayush Jaiswal <aay7ush@gmail.com>
2023-12-25 19:29:36 +0530

    feat: add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer"
+import Header from "@/components/Header"
 import type { Metadata } from "next"
 import "./globals.css"
 
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
         <Footer />
       </body>
---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <header className="fixed w-full z-50 top-0 flex justify-between items-center p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
+      <Link href="/" className="mr-10">
+        <h2 className="text-3xl font-bold">MagicWorld</h2>
+      </Link>
+
+      <div className="flex gap-x-2">
+        {/* <GenreSelect /> */}
+        {/* <Search /> */}
+        {/* <ThemeToggle /> */}
+      </div>
+    </header>
+  )
+}
