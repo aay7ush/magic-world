e5dfe74d9db7cb62cb1bbe4812437b1c4833d32b
Aayush Jaiswal <aay7ush@gmail.com>
2023-12-26 18:07:15 +0530

    Revert today's commits

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link"
-import GenreDropdown from "./GenreDropdown"
 import Search from "./Search"
 import { ThemeToggle } from "./ThemeToggle"
 
-export default async function Header() {
+export default function Header() {
   return (
     <header className="fixed w-full z-50 top-0 flex justify-between items-center p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900">
       <Link href="/" className="mr-10">
@@ -11,7 +10,7 @@ export default async function Header() {
       </Link>
 
       <div className="flex gap-x-2">
-        <GenreDropdown />
+        {/* <GenreSelect /> */}
         <Search />
         <ThemeToggle />
       </div>
